d2ea53471dac813cc38ba2506785e3dac8a743f5
KiranR1605 <kiranr1605@gmail.com>
2023-07-21 20:10:24 +0530

    updated about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -47,7 +47,7 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
       >
-        I'm a skilled computer science student with knowledge of HTML, CSS, JavaScript, and frameworks like React, Node.js, and Next.js. I'm a quick learner and can collaborate closely with others to create efficient and user-friendly solutions that solve real-world problems. Let's work together to bring new ideas to life!
+        I'm a skilled computer science engineering student with knowledge of HTML, CSS, JavaScript, Python, C, Java and frameworks like BootStrap, Flask, React, Node.js, and Next.js. I'm a quick learner and can collaborate closely with others to create efficient and user-friendly solutions that solve real-world problems. Let's work together to bring new ideas to life!
       </motion.p>
 
       <div className='mt-20 flex flex-wrap gap-10'>
